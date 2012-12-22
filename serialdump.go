6c0866fb77125fc8025b7444980cd16384ddde1c
George Redinger <george@georgeredinger.com>
2012-12-21 20:43:09 -0800

    read and dump serial data, non blocking

--- serialdump.go
+++ serialdump.go
@@ -1,9 +1,23 @@
 package main
-
+import "github.com/ziutek/serial"
 import "fmt"
+import "log"
 
 func main() {
-  fmt.Printf("dump\n")
-
+	var n int
+  fmt.Printf("open\n")
+	s, err := serial.Open("/dev/ttyUSB0")
+	if err != nil {
+		log.Fatal(err)
+	}
+  fmt.Printf("begin\n")
+	buf := make([]byte, 128)
+	for {
+		n, err = s.Read(buf)
+		if err != nil {
+			fmt.Printf("%v\n",err)
+		}
+		fmt.Printf("%q", buf[:n])
+	}
 }
 
