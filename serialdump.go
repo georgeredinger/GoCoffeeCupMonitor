05446a7e20ad67c97cd86c9dce937512a6a9258b
George Redinger <george@georgeredinger.com>
2012-12-22 21:10:54 -0800

    separate records

--- serialdump.go
+++ serialdump.go
@@ -1,23 +1,39 @@
 package main
+
 import "github.com/ziutek/serial"
 import "fmt"
 import "log"
+import "time"
 
+
+var	offset int  = 0
 func main() {
 	var n int
-  fmt.Printf("open\n")
+
 	s, err := serial.Open("/dev/ttyUSB0")
 	if err != nil {
 		log.Fatal(err)
 	}
-  fmt.Printf("begin\n")
-	buf := make([]byte, 128)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("begin\n")
+	buf := make([]byte, 16)
 	for {
 		n, err = s.Read(buf)
 		if err != nil {
-			fmt.Printf("%v\n",err)
+			continue
 		}
-		fmt.Printf("%q", buf[:n])
+		if n != 0 {
+		}
+
+			if buf[0] == byte(0x7E) {
+				fmt.Printf("\n")
+			}
+
+			fmt.Printf("%X", buf[:n])
+		
+		time.Sleep(1 * time.Second)
 	}
 }
 
